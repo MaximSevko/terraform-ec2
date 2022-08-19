a18dec511b94a35c6abcebd19cc942d511f65998
Maxim <72321151+MaximSevko@users.noreply.github.com>
2022-08-19 06:13:48 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,12 +3,13 @@ module "ec2" {
   
     name = var.ec2_name
   
+    ami = data.aws_ami.amazon_linux.id
     instance_type               = "t4g.micro"
     availability_zone           = element(module.vpc.azs, 0)
     subnet_id                   = element(module.vpc.public_subnets, 0)
     vpc_security_group_ids      = [module.security_group.security_group_id]
     associate_public_ip_address = true
-  
+    key_name = "amikey"
     root_block_device = [
     {
       encrypted   = true
@@ -20,15 +21,22 @@ module "ec2" {
     },
   ]
 
-  ebs_block_device = [
-    {
-      device_name = "/dev/sdb"
-      volume_type = "gp3"
-      volume_size = 3
-      encrypted   = true
-      kms_key_id  = aws_kms_key.this.arn
-    }
-  ]
+#  ebs_block_device = [
+#    {
+#      device_name = "/dev/sdb"
+#      volume_type = "gp3"
+#      volume_size = 3
+#      encrypted   = true
+#      kms_key_id  = aws_kms_key.this.arn
+#    },
+#    {
+#      device_name = "/dev/sdc"
+#      volume_type = "gp3"
+#      volume_size = 2
+#      encrypted   = true
+#      kms_key_id  = aws_kms_key.this.arn
+#    }
+#  ]
 
 
 
@@ -41,9 +49,12 @@ module "ec2" {
   instance_id = module.ec2.id
 }
 
-resource "aws_ebs_volume" "this" {
+resource "aws_ebs_volume" "disk1" {
   availability_zone = local.availability_zone
-  size              = 1
+  size              = 2
 
   tags = var.volume_tags
+}
+
+resource "aws_kms_key" "this" {
 }
