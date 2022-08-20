6e5e9427d41a52f8c1c6d6f65ad7112be9164b91
Maxim <72321151+MaximSevko@users.noreply.github.com>
2022-08-20 05:43:34 +0300

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -7,7 +7,7 @@ module "security_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
 
   tags = var.securitygroup_tags
