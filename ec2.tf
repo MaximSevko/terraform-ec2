6155f08ef75fcf8ef6dd2f942d0d17d853554206
Maxim <72321151+MaximSevko@users.noreply.github.com>
2022-08-21 06:17:25 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,7 +14,7 @@ module "ec2_instance" {
     monitoring                  = true
     user_data = file("mount")
 
-    iam_instance_profile = aws_iam_instance_profile.test
+    iam_instance_profile = aws_iam_instance_profile.test.name
 
     root_block_device = [
     {
