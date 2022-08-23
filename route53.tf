3e6c21656527c9a10e27961f8399310057e44afc
Maxim <72321151+MaximSevko@users.noreply.github.com>
2022-08-23 05:12:04 +0300

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -4,21 +4,21 @@ resource "aws_route53_zone" "dev" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.dev.zone_id
   name    = "www.example.com"
   type    = "A"
   ttl     = 300
-  records = [aws_eip.lb.public_ip]
+  records = [ec2_instance.public_ip]
 }
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.dev.zone_id
   name    = "www.example.com"
   type    = "A"
   ttl     = 300
-  records = [aws_eip.lb.public_ip]
+  records = [ec2_instance.public_ip]
 }
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.dev.zone_id
   name    = "www.example.com"
   type    = "A"
   ttl     = 300
