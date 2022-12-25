0231727bb30fe2fd5ff3fb4a7c23d13ac467b5ec
Maxim <72321151+MaximSevko@users.noreply.github.com>
2022-12-25 03:12:53 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -48,6 +48,14 @@ variable "ec2_name" {
 
 
 
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+  }
+}
+
 
 
 variable "vpc_tags" {
