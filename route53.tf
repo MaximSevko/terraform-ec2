f769598793dcebdd00cfffb26d14832a1d5920cb
Maxim <72321151+MaximSevko@users.noreply.github.com>
2024-04-15 15:04:37 +0200

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,7 +1,3 @@
-resource "aws_route53_zone" "dev" {
-  name = var.route53_zone_name
-  tags = var.route53_tags
-}
 
 resource "aws_route53_record" "record1" {
   zone_id = aws_route53_zone.dev.zone_id
